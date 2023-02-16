1f16aca9911daa3e5c12a73834efd8beff69f26f
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-02-16 03:49:44 -0800

    update script with the unistd.h's write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -11,6 +11,6 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, sizeof(char), sizeof(str), stderr);
+	write(2, str, sizeof(str));
 	return (1);
 }

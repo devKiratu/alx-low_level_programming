9d8dc36131500a9877a502b9a12093c7a8419a26
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-02-16 00:53:25 -0800

    add a c program to demonstrate puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: program to demonstrate use of puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

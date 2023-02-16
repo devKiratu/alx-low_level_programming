e4859f92e75d557d78a4ec6f26f75100262069a8
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-02-16 04:14:05 -0800

    include string.h file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <stdio.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, str, strlen(str));
 	return (1);

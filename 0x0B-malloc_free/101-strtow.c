441e30b77531ad78cd2fbd55b999c514be376f75
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-03-14 23:42:49 +0300

    add 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,81 @@
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * strtow - splits a string @str into an array of words
+ *
+ * @str: array to split
+ *
+ * Return: pointer to the created strings array, or NULL if operation fails
+ */
+
+char **strtow(char *str)
+{
+	int i = 0, size, len;
+	char **ptr;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	size = get_word_count(str);
+	ptr = malloc((sizeof(char) * size) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		while (isspace(*str))
+			str++;
+		if (*str != '\0')
+		{
+			char *start = str;
+
+			while (*str != '\0' && !isspace(*str))
+				str++;
+
+			len = str - start;
+			ptr[i] = malloc((sizeof(char) * len) + 1);
+			if (ptr[i] == NULL)
+			{
+				while (i >= 0)
+				{
+					free(ptr[i]);
+					i--;
+				}
+				free(ptr);
+				return (NULL);
+			}
+			strncpy(ptr[i], start, len);
+			i++;
+		}
+	}
+	ptr[i] = NULL;
+	return (ptr);
+}
+
+/**
+ * get_word_count - gets the number of words present in a string @str
+ *
+ * @str: string to check
+ *
+ * Return: number of words present
+ */
+
+int get_word_count(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+		str++;
+
+	if (*str != '\0')
+	{
+		while (!isspace(*str))
+			str++;
+		count++;
+	}
+
+	return (count);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,5 +10,7 @@ char *concat_two(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int get_word_count(char *str);
 
 #endif

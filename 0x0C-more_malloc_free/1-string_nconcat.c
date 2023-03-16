5cc864833f214c80772037dddf2fbe644b476ae1
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-03-16 16:50:46 +0300

    allocate single byte when both strings are NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL && s2 == NULL)
 	{
-		ptr = malloc(n);
+		ptr = malloc(1);
 		if (ptr == NULL)
 			return (NULL);
 		ptr[0] = '\0';

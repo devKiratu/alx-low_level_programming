433f3cac234af2808d9321b5f6936263be24f29b
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-03-20 18:18:32 +0300

    document struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct containing a dog's bio
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 struct dog
 {
 	char *name;

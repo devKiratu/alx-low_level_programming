6c17f099f82c17f2028d511a3959919ec01b8051
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-03-30 13:51:29 +0300

    add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - prints a message bfore main is executed
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

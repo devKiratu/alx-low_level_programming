41ba6d95e0a72eac1077cec17130db52a6038ced
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-04-06 15:29:34 +0300

    return correct value (1) when program executes successfully

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit - sets the @indexth bit in @n to 1
  * @n: pointer to unsigned long integer
  * @index: position to set value as 1 in @*n
- * Return: new value of @n or -1 if error occurred
+ * Return: 1 if successful or -1 if error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
@@ -16,5 +16,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 
 	*n |= (1 << index);
-	return (*n);
+	return (1);
 }

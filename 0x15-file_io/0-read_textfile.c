6ee6bd2e2f936558078a2ea2fb396ef02e707cda
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-04-10 09:47:43 +0300

    handle edge case where letters == 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd == -1 || letters == 0)
 		return (0);
 
 	buf = malloc(sizeof(char) * letters);

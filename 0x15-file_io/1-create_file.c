e2b03be3646b0c78edd3c0fdb9bddcc146c7c63a
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-04-10 12:29:47 +0300

    add 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file if it doesn't exist, writes @text_content to it
+ * @filename: name of the file to be created
+ * @text_content: text to write to the created file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, status, o_flags, permissions;
+
+	o_flags = O_WRONLY | O_CREAT | O_EXCL | O_TRUNC;
+	permissions = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, o_flags, permissions);
+	if (fd == -1)
+	{
+		fd = open(filename, O_WRONLY);
+		if (fd == -1)
+			return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		status = write(fd, text_content, strlen(text_content) + 1);
+		if (status == -1)
+			return (-1);
+	}
+
+	return (1);
+}

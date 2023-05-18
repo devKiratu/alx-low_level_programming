9089a32fb99444b210dca3220f3e4d964d6a5c46
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-18 14:54:35 +0300

    enable insertion at head

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,6 +19,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next = NULL;
 	new->prev = NULL;
 
+	if (idx == 0)
+	{
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+
 	temp = *h;
 	while (temp->next != NULL)
 	{

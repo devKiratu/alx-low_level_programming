55eaa4e3d3bd405174a5603e44e0092307cbbba3
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-05-22 11:47:13 +0300

    add 100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - adds two integers @a and @b
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts @b from @a
+ * @a: first integer
+ * @b: second integer
+ * Return: result of @a - @b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers @a and @b
+ * @a: first integer
+ * @b: second integer
+ * Return: product of @a and @b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides @a by @b
+ * @a: dividend
+ * @b: divisor
+ * Return: result of @a / @b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - calculates the remainder of @a % @b
+ * @a: first integer
+ * @b: second integer
+ * Return: result of @a % @b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
